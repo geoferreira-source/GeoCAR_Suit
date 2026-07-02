4669d73d36e836f8631a6e550272749631fa7626
geoferreira-source <geo.miguelferreira@gmail.com>
2026-07-02 16:10:16 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""GeoCAR Suite — Plugin QGIS | Miguel A. L. Ferreira"""
-
-def classFactory(iface):
-    from .car_analyzer import GeoCARSuitePlugin
-    return GeoCARSuitePlugin(iface)
